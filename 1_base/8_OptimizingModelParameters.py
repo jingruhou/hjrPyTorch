90271e54532ecc1b0e5afe4803ed57272b826c34
1627383642@qq.com <Hjr124077>
2021-05-06 16:14:11 +0800

    优化模型参数

--- 1_base/8_OptimizingModelParameters.py
+++ 1_base/8_OptimizingModelParameters.py
@@ -100,13 +100,14 @@ def test_loop(dataloader, model, loss_fn):
 
     test_loss /= size
     correct /= size
-    print(f"Test Error :\ Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss: >8f} \n")
+    print(f"Test Error :\n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss: >8f} \n")
 
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 epochs = 10
+
 for t in range(epochs):
     print(f"Epoch {t + 1} \n --------------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
